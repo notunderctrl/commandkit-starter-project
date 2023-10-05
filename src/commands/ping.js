73b356f4f7fac6afa59cb7b44a1bd8b6ab12deae
Avraj Sahota <underctrl.io@gmail.com>
2023-10-05 17:22:22 +0300

    update commandkit version

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,10 +1,17 @@
+/** @type {import('commandkit').CommandData} */
 const data = {
     name: 'ping',
     description: 'Pong!',
 };
 
+/** @param {import('commandkit').SlashCommandProps} param0 */
 function run({ interaction, client }) {
     interaction.reply(`:ping_pong: Pong! ${client.ws.ping}ms`);
 }
 
-module.exports = { data, run };
+/** @type {import('commandkit').CommandOptions} */
+const options = {
+    devOnly: true,
+};
+
+module.exports = { data, run, options };
